4048f031865d8bd7ecebb52dacfdbfb2decd7e99
Noah Wagman <noah-wagman@Mac.lan>
2026-02-07 00:40:51 -0500

    ignore ts errors during build

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@visit/ui", "@visit/lib", "@visit/db"],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   async rewrites() {
     return [
       { source: '/checkin', destination: '/checkin.html' },
